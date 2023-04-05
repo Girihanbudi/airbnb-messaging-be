b7d896fa911db5535017837f019ddfa3727f0125
girihanbudi <Ghanbudi@gmail.com>
2023-04-05 23:43:18 +0700

    added unavailable standard error preset

--- internal/pkg/stderror/preset.go
+++ internal/pkg/stderror/preset.go
@@ -4,6 +4,7 @@ import "net/http"
 
 var (
 	DEF_SERVER_500 = New(http.StatusUnauthorized, "DEF_SERVER_500", "Failed to get message")
+	DEF_SERVER_503 = New(http.StatusServiceUnavailable, "DEF_SERVER_503", "Service unavailable to response")
 	DEF_AUTH_401   = New(http.StatusUnauthorized, "DEF_AUTH_401", "Failed to authorize user")
 	DEF_DATA_400   = New(http.StatusBadRequest, "DEF_DATA_400", "Failed to bind request")
 )
