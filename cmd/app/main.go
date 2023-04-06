eabcd94ae0d585d3e5911132c59eaa813756a59e
girihanbudi <Ghanbudi@gmail.com>
2023-04-06 09:47:26 +0700

    created messaging app

---
+++ cmd/app/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"airbnb-messaging-be/internal/app"
+	"airbnb-messaging-be/internal/pkg/env"
+	"context"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+)
+
+// @title           Airbnb User Backend API
+// @version         1.0
+// @description     Airbnb User Backend Service API
+// @termsOfService  https://airbnb.co.id
+
+// @contact.name   API Support
+// @contact.url    https://airbnb.co.id/support
+// @contact.email  support@airbnb.co.id
+
+// @host      localhost:8080
+// @BasePath  /v1
+
+// @securityDefinitions.basic BasicAuth
+func main() {
+	// init app environment
+	defaultEnvConfig := env.NewDefaultEnvConf()
+	env.InitEnv(defaultEnvConfig)
+
+	// create app context
+	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+
+	// create service app
+	serviceApp, err := app.NewApp()
+	if err != nil {
+		log.Panic(err)
+	}
+
+	serviceApp.Run(ctx)
+	stop()
+}
