edec048ea5d623769bee8a8a1cbb10c97519d034
girihanbudi <Ghanbudi@gmail.com>
2023-04-08 23:27:01 +0700

    updated logger output

--- internal/pkg/log/log.go
+++ internal/pkg/log/log.go
@@ -1,22 +1,61 @@
 package log
 
-import log "github.com/sirupsen/logrus"
+import (
+	"github.com/sirupsen/logrus"
+	prefixed "github.com/x-cray/logrus-prefixed-formatter"
+)
 
-const key = "Instance"
+var log = logrus.New()
+
+func init() {
+	formatter := new(prefixed.TextFormatter)
+	formatter.FullTimestamp = true
+
+	// Set specific colors for prefix and timestamp
+	formatter.SetColorScheme(&prefixed.ColorScheme{
+		PrefixStyle:    "blue+b",
+		TimestampStyle: "white+h",
+	})
+
+	log.Formatter = formatter
+	log.Level = logrus.DebugLevel
+}
+
+func NewLogger(instance string, removeNewLine bool) *logrus.Entry {
+	newLogger := log
+	entry := newLogger.WithFields(logrus.Fields{
+		"prefix": instance,
+	})
+
+	if removeNewLine {
+		if len(entry.Message) > 0 && entry.Message[len(entry.Message)-1] == '\n' {
+			b := []byte(entry.Message[0 : len(entry.Message)-1])
+			entry.Message = string(b)
+		}
+		newLogger.Formatter.Format(entry)
+	}
+
+	return entry
+}
 
 func Event(instance, msg string) {
 	if instance != "" {
-		log.WithField(key, instance).Info(msg)
+		log.WithFields(logrus.Fields{
+			"prefix": instance,
+		}).Info(msg)
 	} else {
 		log.Info(msg)
 	}
-
 }
 
 func Fatal(instance, msg string, err error) {
-	log.WithField(key, instance).Fatal(msg, ": ", err.Error())
+	log.WithFields(logrus.Fields{
+		"prefix": instance,
+	}).Fatal(msg, ": ", err.Error())
 }
 
 func Error(instance, msg string, err error) {
-	log.WithField(key, instance).Errorln(msg, ": ", err.Error())
+	log.WithFields(logrus.Fields{
+		"prefix": instance,
+	}).Errorln(msg, ": ", err.Error())
 }
