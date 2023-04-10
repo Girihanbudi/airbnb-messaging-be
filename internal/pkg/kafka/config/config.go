fd19ed9470a69a744b7d6015172dd87803741674
girihanbudi <Ghanbudi@gmail.com>
2023-04-10 10:25:46 +0700

    refactor sarama client

--- internal/pkg/kafka/config/config.go
+++ internal/pkg/kafka/config/config.go
@@ -1,15 +1,12 @@
 package config
 
 import (
-	router "airbnb-messaging-be/internal/pkg/kafka/router/config"
+	consumer "airbnb-messaging-be/internal/pkg/kafka/consumer/config"
 )
 
 type Config struct {
-	ClientId    string        `mapstructure:"clientid"`
-	Brokers     []string      `mapstructure:"brokers"`
-	Group       string        `mapstructure:"group"`
-	Assigner    string        `mapstructure:"assigner"`
-	Version     string        `mapstructure:"version"`
-	IsUseOldest bool          `mapstructure:"isuseoldest"`
-	Router      router.Config `mapstructure:"router"`
+	ClientId string          `mapstructure:"clientid"`
+	Brokers  []string        `mapstructure:"brokers"`
+	Version  string          `mapstructure:"version"`
+	Consumer consumer.Config `mapstructure:"consumer"`
 }
--- internal/pkg/kafka/provider.go
+++ internal/pkg/kafka/provider.go
@@ -4,31 +4,23 @@ import (
 	"airbnb-messaging-be/internal/pkg/kafka/config"
 	"airbnb-messaging-be/internal/pkg/kafka/router"
 	"airbnb-messaging-be/internal/pkg/log"
-	"fmt"
 
 	"github.com/Shopify/sarama"
 )
 
-const Instance string = "Kafka Client"
-
-type TopicHandler struct {
-	Topic   string
-	Handler func()
-}
+const Instance string = "Sarama Client"
 
 type Options struct {
 	config.Config
 	Router *router.Router
 }
 
-type Listener struct {
-	isReady chan bool
-
-	Consumer sarama.ConsumerGroup
+type Client struct {
+	Config *sarama.Config
 	Options
 }
 
-func NewEventListener(options Options) *Listener {
+func NewSaramaClient(options Options) *Client {
 
 	sarama.Logger = log.NewLogger(Instance, false)
 	version, err := sarama.ParseKafkaVersion(options.Version)
@@ -44,31 +36,8 @@ func NewEventListener(options Options) *Listener {
 	config.Version = version
 	config.ClientID = options.ClientId
 
-	switch options.Assigner {
-	case "sticky":
-		config.Consumer.Group.Rebalance.GroupStrategies = []sarama.BalanceStrategy{sarama.BalanceStrategySticky}
-	case "roundrobin":
-		config.Consumer.Group.Rebalance.GroupStrategies = []sarama.BalanceStrategy{sarama.BalanceStrategyRoundRobin}
-	case "range":
-		config.Consumer.Group.Rebalance.GroupStrategies = []sarama.BalanceStrategy{sarama.BalanceStrategyRange}
-	default:
-		log.Fatal(Instance, fmt.Sprintf("unrecognized consumer group partition assignor: %s", options.Assigner), nil)
-	}
-
-	if options.IsUseOldest {
-		config.Consumer.Offsets.Initial = sarama.OffsetOldest
-	}
-
-	client, err := sarama.NewConsumerGroup(options.Brokers, options.Group, config)
-	if err != nil {
-		log.Fatal(Instance, "error creating consumer group client: %v", err)
-	}
-
-	log.Event(Instance, fmt.Sprintf("ready to consume from %v", options.Brokers))
-
-	return &Listener{
-		isReady:  make(chan bool),
-		Consumer: client,
-		Options:  options,
+	return &Client{
+		Config:  config,
+		Options: options,
 	}
 }
