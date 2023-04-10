f0f96e175d91b1f1313f04fdb5fb40f2086705d1
girihanbudi <Ghanbudi@gmail.com>
2023-04-10 10:26:43 +0700

    added tls configuration creator

--- internal/pkg/http/engine.go
+++ internal/pkg/http/engine.go
@@ -2,7 +2,11 @@ package http
 
 import (
 	"airbnb-messaging-be/internal/pkg/env"
+	"crypto/tls"
+	"crypto/x509"
+	"log"
 	"net/http"
+	"os"
 )
 
 func DefaultSameSite() http.SameSite {
@@ -11,4 +15,34 @@ func DefaultSameSite() http.SameSite {
 	} else {
 		return http.SameSiteNoneMode
 	}
+}
+
+func CreateTlsConfiguration(certFile, keyFile, caFile string, tlsSkipVerify bool) (t *tls.Config) {
+	// "certificate", "", "The optional certificate file for client authentication"
+	// "key", "", "The optional key file for client authentication"
+	// "ca", "", "The optional certificate authority file for TLS client authentication"
+	// "tls-skip-verify", false, "Whether to skip TLS server cert verification"
+
+	if certFile != "" && keyFile != "" && caFile != "" {
+		cert, err := tls.LoadX509KeyPair(certFile, keyFile)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		caCert, err := os.ReadFile(caFile)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		caCertPool := x509.NewCertPool()
+		caCertPool.AppendCertsFromPEM(caCert)
+
+		t = &tls.Config{
+			Certificates:       []tls.Certificate{cert},
+			RootCAs:            caCertPool,
+			InsecureSkipVerify: tlsSkipVerify,
+		}
+	}
+	// will be nil by default if nothing is provided
+	return t
 }
