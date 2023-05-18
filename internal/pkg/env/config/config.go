6bbc6bf3c44675e59fc3e3a706a79dbc6ddcb138
girihanbudi <Ghanbudi@gmail.com>
2023-05-18 22:21:55 +0700

    updated missing env

--- internal/pkg/env/config/config.go
+++ internal/pkg/env/config/config.go
@@ -2,6 +2,8 @@ package config
 
 import (
 	cache "airbnb-messaging-be/internal/pkg/cache/config"
+	credential "airbnb-messaging-be/internal/pkg/credential/config"
+	elastic "airbnb-messaging-be/internal/pkg/elasticsearch/config"
 	gorm "airbnb-messaging-be/internal/pkg/gorm/config"
 	httpserver "airbnb-messaging-be/internal/pkg/http/server/config"
 	jwt "airbnb-messaging-be/internal/pkg/jwt/config"
@@ -13,10 +15,12 @@ type Config struct {
 	Stage      string             `mapstructure:"stage"`
 	Origins    []string           `mapstructure:"origins"`
 	Domain     string             `mapstructure:"domain"`
+	Creds      credential.Config  `mapstructure:"creds"`
 	HttpServer httpserver.Config  `mapstructure:"httpserver"`
 	DB         gorm.Config        `mapstructure:"db"`
 	Jwt        jwt.Config         `mapstructure:"jwt"`
 	Cache      cache.Config       `mapstructure:"cache"`
-	Messenger  messagebird.Config `mapstructure:"messenger"`
+	Elastic    elastic.Config     `mapstructure:"elastic"`
 	Kafka      kafka.Config       `mapstructure:"kafka"`
+	Messenger  messagebird.Config `mapstructure:"messenger"`
 }
